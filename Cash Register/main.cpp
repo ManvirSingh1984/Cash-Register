f830d2a0d0c966b500d53c008c4209fe66b52d0c
Manvir Singh <manvirsinghak47@gmail.com>
2021-05-01 23:13:08 +0530

    Initial Commit

---
+++ Cash Register/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cash Register
+//
+//  Created by Manvir Singh on 01/05/21.
+//  Copyright © 2021 Manvir Singh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
